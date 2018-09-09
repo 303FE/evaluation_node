c845f51de83123b9522c31d208758664f1ebbc67
boen <1997@boenfu.cn>
2018-09-09 19:05:32 +0800

    新增权限验证

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const userService = require('../services/userService')
 const resultHelper = require('../util/resultHelper')
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+
+router.get('/login', function(req, res, next) {
   userService
     .login({type: 2, num: '159001015', password: '123'})
     .then(rel =>{
@@ -11,6 +11,28 @@ router.get('/', function(req, res, next) {
         res.send(resultHelper.success(rel, '成功'))
       } else {
         res.send(resultHelper.failed( '账号或密码错误'))
+      }
+    })
+    .catch(() =>{
+      res.send(resultHelper.failed('系统错误'))
+    })
+
+});
+
+router.post('/changePassword', function(req, res) {
+  let params = Object.assign({
+      type: 0,
+      id: '',
+      password: '',
+      newPass: ''
+    }, req.body)
+  userService
+    .changePassword(params)
+    .then(rel =>{
+      if (rel) {
+        res.send(resultHelper.success({}, '修改成功'))
+      } else {
+        res.send(resultHelper.failed( '修改失败'))
       }
     })
     .catch(() =>{
